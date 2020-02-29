67235444c40956b4a543694fce343730036c6d7c
Alexander Lamp <alexander.mlamp@gmail.com>
2020-02-29 15:57:19 -0800

    updating repo

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 CREATE TABLE employees (
-	     emp_no INT NOT NULL,
+	 emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -20,8 +20,7 @@ dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE salaries (
